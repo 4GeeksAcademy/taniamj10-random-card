cecdef1b01babec907556c1ed661565a7ab0fd44
Tania Madrigal <129007972+TaniaMJ10@users.noreply.github.com>
2023-07-14 20:41:02 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -4,8 +4,41 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { defaultModifiers } from "@popperjs/core/lib/popper-lite";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // declaro los datos
+  //esto es un array, que es una lista de datos, puede ser en cualquier orden
+  let numero = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  //esto es un array de objetos, porque los datos tienen propiedad y valor
+  let simbolo = [
+    { S: "♦", C: "red" },
+    { S: "♥", C: "red" },
+    { S: "♠", C: "black" },
+    { S: "♣", C: "black" }
+  ];
+  //hago la logica
+  let numero1 = numero[Math.floor(Math.random() * numero.length)];
+  let simbolo1 = simbolo[Math.floor(Math.random() * simbolo.length)];
+  //llamo a la logica para que lo pinte en el Html
+  document.querySelector(".letra").innerHTML = numero1;
+  document.querySelectorAll(".icono").forEach((elem, index) => {
+    elem.innerHTML = simbolo1.S;
+    elem.style.color = simbolo1.C;
+  });
 };
